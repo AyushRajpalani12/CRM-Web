9955605b3ec9b0bcd8dfe8cbba3d7a67f1ec8e96
AyushRajpalani12 <rajpalaniayush72@gmail.com>
2025-12-18 11:47:27 +0530

    Second  commit

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -7,7 +7,7 @@ import Fourth from "./Fourth";
 import Fifth from "./Fifth";
 import Sixth from "./Sixth";
 import Footer from "./Footer";
-import JewelleryPage from "./JewelllaryPage";
+// import JewelleryPage from "./JewelllaryPage";
 
 function Home() {
   return (
@@ -21,7 +21,7 @@ function Home() {
       <Sixth />
       {/* <Seventh /> */}
       <Footer />
-      <JewelleryPage/>
+      {/* <JewelleryPage/> */}
     </>
   );
 }
